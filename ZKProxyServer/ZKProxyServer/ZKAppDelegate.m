f5e14f3e4b83b8800d5d3d4216ca0ed68172822f
Zeeshan Khan <zeeshan.s.khan@gmail.com>
2014-08-25 18:06:53 +0530

    Unnecessary code removed.

--- ZKProxyServer/ZKProxyServer/ZKAppDelegate.m
+++ ZKProxyServer/ZKProxyServer/ZKAppDelegate.m
@@ -10,9 +10,7 @@
 #import "ZKProxyServer.h"
 #import "ZKRequestCall.h"
 
-@interface ZKAppDelegate () {
-    NSMutableData *responseData;
-}
+@interface ZKAppDelegate ()
 @property (nonatomic, strong) NSOperationQueue *requestQueue;
 @end
 
@@ -59,95 +57,10 @@
 }
 
 - (void)callWebService {
-    NSLog(@"Call Web Service Button Clicked");
 
-    // NSOperation calling.. make sure you only call this, comment below calling.
-//    ZKRequestCall *request1 = [ZKRequestCall new];
-//    [self.requestQueue addOperation:request1];
-//    [request1 release]; request1 = nil;
-    
-    
-    
-    NSLog(@"Main Thread calling...");
-    if (responseData == nil) {
-
-        NSURL *url = [NSURL URLWithString:@"http://127.0.0.1:8080/"];
-        NSMutableURLRequest *request = [[[NSMutableURLRequest alloc] initWithURL:url] autorelease];
-        [request setHTTPBody:[@"Request Body Data" dataUsingEncoding:NSUTF8StringEncoding]];
-        [request setHTTPMethod:@"POST"];
-        
-        
-        // 1. It didn't work for any.
-//        [NSURLConnection connectionWithRequest:request delegate:self];
-        
-        
-        // 2. It didn't work for large image file, tried with 18MB.
-        NSURLConnection *con = [[NSURLConnection alloc] initWithRequest:request delegate:self startImmediately:NO];
-        [con setDelegateQueue:[NSOperationQueue currentQueue]];
-        [con start];
-
-        
-        // 3. It didn't work for large image file, tried with 18MB.
-//        NSURLConnection *con = [[NSURLConnection alloc] initWithRequest:request delegate:self startImmediately:NO];
-//        [con scheduleInRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode]; // Or NSRunLoopCommonModes
-//        [con start];
-
-        
-        // 4. It didn't work for large image file, tried with 18MB.
-//        NSURLConnection *con = [[NSURLConnection alloc] initWithRequest:request delegate:self startImmediately:NO];
-//        NSRunLoop *loop = [NSRunLoop currentRunLoop];
-//        [con scheduleInRunLoop:loop forMode:NSRunLoopCommonModes];
-//        [con start];
-//        [loop run]; // need to close this.
-        
-        
-        // 5. Crash (need to look into)
-//        NSHTTPURLResponse *res = nil;
-//        NSError *err = nil;
-//        NSData *data = [NSURLConnection sendSynchronousRequest:request returningResponse:&res error:&err];
-//        NSLog(@"Res Code: %ld, DataLen: %lu", (long)res.statusCode, (unsigned long)data.length);
-        
-        
-        // 6. WORKED for large image as well
-//        [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue currentQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
-//            NSHTTPURLResponse *res = (NSHTTPURLResponse*)response;
-//            NSString *imagePath = [[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0] stringByAppendingPathComponent:@"/myImage.jpg"];
-//            NSLog(@"Res Code: %ld, DataLen: %lu, Path: %@", (long)res.statusCode, (unsigned long)data.length, imagePath);
-//            [data writeToFile:imagePath atomically:YES];
-//        }];
-
-        responseData = [NSMutableData new];
-    }
+    ZKRequestCall *request1 = [ZKRequestCall new];
+    [self.requestQueue addOperation:request1];
+    [request1 release]; request1 = nil;
 }
-
-#pragma mark - Connection Delegate
-
-- (void) connection:(NSURLConnection *) connection didReceiveResponse:(NSURLResponse *) response {
-    
-    NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
-    NSInteger responseStatusCode = httpResponse.statusCode;
-    NSLog(@"Response Status Code: %ld", (long)responseStatusCode);
-}
-
-- (void) connection:(NSURLConnection *) connection didReceiveData:(NSData *) data {
-    [responseData appendData:data];
-}
-
-- (void) connection:(NSURLConnection *) connection didFailWithError:(NSError *) error {
-    NSLog(@"Error Code: %@", error);
-    [responseData release]; responseData = nil;
-}
-
-- (void) connectionDidFinishLoading:(NSURLConnection *) connection  {
-    
-    NSString *imagePath = [[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0] stringByAppendingPathComponent:@"/myImage.jpg"];
-    NSLog(@"Img path: %@, DataLen: %lu", imagePath, (unsigned long)responseData.length);
-    [responseData writeToFile:imagePath atomically:YES];
-
-//    NSString *strIncomingData = [[[NSString alloc] initWithData:responseData encoding:NSUTF8StringEncoding] autorelease];
-//    NSLog(@"Response: %@", strIncomingData);
-    [responseData release]; responseData = nil;
-}
-
 
 @end
--- ZKProxyServer/ZKProxyServer/ZKRequestCall.m
+++ ZKProxyServer/ZKProxyServer/ZKRequestCall.m
@@ -29,52 +29,10 @@
     [request setHTTPBody:[@"Request Body Data" dataUsingEncoding:NSUTF8StringEncoding]];
     [request setHTTPMethod:@"POST"];
 
-    // 1. Nothing worked (None of the delegate method get called.)
-//    [NSURLConnection connectionWithRequest:request delegate:self];
-
-    
-    // 2. It WORKED for all.
-//    NSURLConnection *con = [[NSURLConnection alloc] initWithRequest:request delegate:self startImmediately:NO];
-//    [con setDelegateQueue:[NSOperationQueue currentQueue]];
-//    [con start];
-
-
-    // 3A. It didn’t work for large file.
-//    NSURLConnection *con = [[NSURLConnection alloc] initWithRequest:request delegate:self startImmediately:NO];
-//    [con scheduleInRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
-//    [con start];
-
-    
-    // 3B. It didn’t work for large file.
-//    NSURLConnection *con = [[NSURLConnection alloc] initWithRequest:request delegate:self startImmediately:NO];
-//    [con scheduleInRunLoop:[NSRunLoop mainRunLoop] forMode:NSRunLoopCommonModes];
-//    [con start];
-    
-    
-    // 4. It WORKED for all. (It worked for NSDefaultRunLoopMode mode as well, where apple document says this “The mode to deal with input sources other than NSConnection objects.”).
-//    NSURLConnection *con = [[NSURLConnection alloc] initWithRequest:request delegate:self startImmediately:NO];
-//    NSRunLoop *loop = [NSRunLoop currentRunLoop];
-//    [con scheduleInRunLoop:loop forMode:NSRunLoopCommonModes];
-//    [con start];
-//    [loop run];   // [loop runUntilDate:[NSDate distantFuture]]; // Need to stop this run loop as well
-
-    
-    // 5. It WORKED for all.
-//    NSHTTPURLResponse *res = nil;
-//    NSError *err = nil;
-//    NSData *data = [NSURLConnection sendSynchronousRequest:request returningResponse:&res error:&err];
-//    NSLog(@"Res Code: %ld, DataLen: %lu", (long)res.statusCode, (unsigned long)data.length);
-
-    
-    // 6. It WORKED for all.
-//    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue currentQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
-//        NSHTTPURLResponse *res = (NSHTTPURLResponse*)response;
-//        NSString *imagePath = [[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0] stringByAppendingPathComponent:@"/myImage.jpg"];
-//        NSLog(@"Res Code: %ld, DataLen: %lu, Path: %@", (long)res.statusCode, (unsigned long)data.length, imagePath);
-//        [data writeToFile:imagePath atomically:YES];
-//    }];
-
-    
+    NSURLConnection *con = [[NSURLConnection alloc] initWithRequest:request delegate:self startImmediately:NO];
+    [con setDelegateQueue:[NSOperationQueue currentQueue]];
+    [con start];
+    [con release]; con = nil;
 }
 
 #pragma mark - Connection Delegate
@@ -109,18 +67,20 @@
     [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
     NSLog(@"ConnectionDidFinish");
 
+    // String Test
 //    NSString *strIncomingData = [[[NSString alloc] initWithData:self.responseData encoding:NSUTF8StringEncoding] autorelease];
-//    NSLog(@"Response String: %@ - %@", self.requestType, strIncomingData);
+//    NSLog(@"Response String: %@", strIncomingData);
+//    return;
 
+    // Image Test
     NSString *imagePath = [[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0] stringByAppendingPathComponent:@"/myImage1.PNG"];
 
     UIImage *img = [UIImage imageWithData:self.responseData];
-    
     NSLog(@"Image is %@, DataLen: %lu, Path: %@", (img == nil)?@"nil":@"valid", (unsigned long)self.responseData.length, imagePath);
     [self.responseData writeToFile:imagePath atomically:YES];
     
+
     self.responseData = nil;
-    
     [connection cancel];
     connection = nil;
 }
